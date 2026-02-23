29875b4122c111b6c6bd5649debc21394184305e
THIAGO COSTA ALVES DA SILVA <25203656@CFP127ALUNO.LOCAL>
2026-02-23 10:17:57 -0300

    Cadastrar-Nome

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const cadastrar = document.getElementById("cadastrar")
+
+function cadastraNome () {
+    const nome = document.getElementById("nome")
+    const listaNomes = document.getElementById("lista-nomes")
+    
+    const itemNome = document.createElement("p")
+    itemNome.textContent = nome.value
+
+    listaNomes.appendChild(itemNome)
+}
+
+cadastrar.addEventListener("click", cadastraNome)
